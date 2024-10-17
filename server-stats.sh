2bb29263ef8546ed09966bbf8325c2feb49ca0b4
natczicro <nczicro@gmail.com>
2024-10-16 23:19:54 -0400

    memory usage

--- server-stats.sh
+++ server-stats.sh
@@ -8,3 +8,6 @@ echo "$(cat /proc/stat |grep cpu |tail -1|awk '{print ($5*100)/($2+$3+$4+$5+$6+$
 
 echo "CPU Usage with top"
 echo "$(top -bn2 | grep "Cpu" | tail -1 | awk '{print "CPU Usage: " 100-$8 "%"}')"
+
+echo "Memory usage with free"
+echo "$(free -m | grep "Mem" | awk '{print "Total = " $2 "MB\nUsed = " $3 "MB\nFree = "$4 "MB\nPercent = " $3/$2*100}')"
