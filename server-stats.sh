c4f627abd1c742277f4c2dc5150fbe9b4837dbe4
natczicro <nczicro@gmail.com>
2024-10-16 22:35:31 -0400

    server stats for CPU usage

---
+++ server-stats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "CPU Usage with vmstat 1 2"
+echo "CPU Usage: "$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]"%"
+
+echo "CPU Usage with /proc/stat"
+echo "$(cat /proc/stat |grep cpu |tail -1|awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'|awk '{print "CPU Usage: " 100-$1}')"
+
+echo "CPU Usage with top"
+echo "$(top -bn2 | grep "Cpu" | tail -1 | awk '{print "CPU Usage: " 100-$8 "%"}')"
