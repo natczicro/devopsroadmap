73a474941957686d8cb3e49eb170f8feb2097365
nccrooks <nczicro@gmail.com>
2024-10-22 17:47:03 -0400

    log analyzer path count

--- log_analyzer/log_analyzer.sh
+++ log_analyzer/log_analyzer.sh
@@ -3,3 +3,7 @@
 echo -e "Top 5 IP addresses with the most requests:"
 
 echo -e "$(cat nginx-access.log | awk '{print $1}' | sort | uniq -c | sort -r | head -n 5 | awk '{print $2 " - " $1 " requests"}')"
+
+echo -e "\nTop 5 most requested paths:"
+
+echo "$(cat nginx-access.log | awk '{print $7}' | sort | uniq -c | grep "/" | sort -r | head -n 5 | awk '{print $2 " - " $1 " requests"}')"
