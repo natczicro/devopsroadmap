2f3762a7da215e5ee1fdd0bb74a12e6bb7e33d3b
nccrooks <nczicro@gmail.com>
2024-10-22 16:11:14 -0400

    pre sort before uniq in order to properly count

--- log_analyzer/log_analyzer.sh
+++ log_analyzer/log_analyzer.sh
@@ -2,4 +2,4 @@
 
 echo -e "Top 5 IP addresses with the most requests:"
 
-echo -e "$(cat nginx-access.log | awk '{print $1}' | uniq -c | sort -r | head -n 5 | awk '{print $2 " - " $1 " requests"}')"
+echo -e "$(cat nginx-access.log | awk '{print $1}' | sort | uniq -c | sort -r | head -n 5 | awk '{print $2 " - " $1 " requests"}')"
