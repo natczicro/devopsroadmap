7797f30a9e0e5aff07f72dd06e36aee079d40200
nccrooks <nczicro@gmail.com>
2024-10-22 16:10:41 -0400

    log analyzer IP counter

---
+++ log_analyzer/log_analyzer.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+echo -e "Top 5 IP addresses with the most requests:"
+
+echo -e "$(cat nginx-access.log | awk '{print $1}' | uniq -c | sort -r | head -n 5 | awk '{print $2 " - " $1 " requests"}')"
