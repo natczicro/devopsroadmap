960c3f4a4eac03dbc7655b365539c4fb4b0f4352
nccrooks <nczicro@gmail.com>
2024-10-22 20:41:05 -0400

    user agent requests grepping on Mozilla since it is at the start of each line

--- log_analyzer/log_analyzer.sh
+++ log_analyzer/log_analyzer.sh
@@ -11,3 +11,7 @@ echo "$(cat nginx-access.log | awk '{print $7}' | sort | uniq -c | grep "/" | so
 echo -e "\nTop 5 returned status codes:"
 
 echo "$(cat nginx-access.log | grep "GET\|POST\|PATCH\|OPTIONS" | awk '{print $9}' | sort |uniq -c | sort -r | head -n 5 | awk '{print $2 " - " $1 " requests"}')"
+
+echo -e "\nTop 5 return user agents:"
+
+echo "$(cat nginx-access.log | awk -F\" '{print $6}' | grep "Mozilla" | sort | uniq -c | sort -r | head -n 5 | awk '{print $2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "$(10)" "$(11)" "$(12)" "$(13) " - "$1 " requests"}')"
